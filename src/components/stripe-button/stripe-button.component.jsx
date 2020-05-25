36b6b56404420fdf0082a910a4c791c61b8f408c
Robin Steeman <Robin_steeman@hotmail.com>
2020-05-25 16:47:12 +0900

    implemented stripe and updated favicon

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+   const priceForStripe = price * 100;
+   const publishableKey = 'pk_test_vO7tmdGGQ8aT1VmMEP6Dakl8007m0xJDrf';
+
+   const onToken = token => {
+      console.log(token);
+      alert('Payment Successful');
+   };
+
+   return (
+      <StripeCheckout
+         currency='EUR'
+         bitcoin
+         label='Pay Now'
+         name='CRWN Clothing Ltd.'
+         billingAddress
+         shippingAddress
+         image='https://svgshare.com/i/CUz.svg'
+         description={`Your total is €${price}`}
+         amount={priceForStripe}
+         panelLabel='Pay Now'
+         token={onToken}
+         stripeKey={publishableKey}
+      />
+   );
+};
+
+export default StripeCheckoutButton;
